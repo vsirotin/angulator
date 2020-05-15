e528d2d3183d8f4a8db206819392f7af958917e6
Viktor Sirotin <vxod@sirotin.eu>
2020-05-15 15:20:34 +0200

    Update main.ts
    JS experiments

--- JavaScriptor/src/main.ts
+++ JavaScriptor/src/main.ts
@@ -12,3 +12,21 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
   console.log("JavaScriptor runs!")
+
+  let f1 = function(name, weather="nice"){
+    console.log("Hi " + name +"! It is " + weather + " weather today!");
+  } 
+  f1("Nick", "good");
+
+  
+
+  let f2 = function(name, weather="nice", ...adds){
+    console.log("Hi " + name +"! It is " + weather + " weather today!");
+
+    for (let i = 0; i < adds.length; i++) {
+      console.log("Add N" + i + ": "  + adds[i]);
+    }
+
+  } 
+
+  f2("John", "starnge", "ranning", "snowing");
