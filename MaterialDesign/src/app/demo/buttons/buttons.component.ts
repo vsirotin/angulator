a8ab206b2dfd1dd5bfb373c0ec106223fcc982ca
Viktor Sirotin <vxod@sirotin.eu>
2020-05-31 14:38:34 +0200

    add material design in demo component

--- MaterialDesign/src/app/demo/buttons/buttons.component.ts
+++ MaterialDesign/src/app/demo/buttons/buttons.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-buttons',
   template: `
-Ax!
+    <button mat-button>
+    <mat-icon>face</mat-icon>
+    </button>
+    <mat-checkbox>Do it!</mat-checkbox>
   `,
   styles: [
   ]
