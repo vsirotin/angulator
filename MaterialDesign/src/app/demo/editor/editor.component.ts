49dcb6d026ffc9a4159e031dedf6b6dbaf2e2766
viktorsirotin <vxod@sirotin.eu>
2021-04-01 11:28:10 +0200

    CKEditor replaced with editable div

--- MaterialDesign/src/app/demo/editor/editor.component.ts
+++ MaterialDesign/src/app/demo/editor/editor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 
 @Component({
   selector: 'app-editor',
@@ -8,11 +8,59 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 })
 export class EditorComponent implements OnInit {
 
-  public Editor = ClassicEditor;
+  public myText 
 
   constructor() { }
 
   ngOnInit(): void {
+    document.querySelector('html').addEventListener('mouseup', function (e) {
+      console.log("html mouseup");
+      var evt = document.createEvent("MouseEvents");
+      evt.initEvent("mouseup", true, true);
+    //  document.getElementById('texa').dispatchEvent(evt);
+  });
   }
 
+
+doChange(event: InputEvent){
+  
+  console.log("Changes: data=" + event.data + " " + event.inputType + " " + this.getPos(document.activeElement))
+}
+  public myFunction() {
+    console.log("SElection=" +  this.getText(document.activeElement));
+  }
+
+  public getPos(elem) {
+    if(elem.tagName === "TEXTAREA" ||
+       (elem.tagName === "INPUT" && elem.type === "text")) {
+        return elem.selectionStart;
+    }
+    return null;
+}
+  
+  public getText(elem) {
+    console.log("In getText elem=", elem)
+      if(elem.tagName === "TEXTAREA" ||
+         (elem.tagName === "INPUT" && elem.type === "text")) {
+          return elem.value.substring(elem.selectionStart,
+                                      elem.selectionEnd);
+      }
+      return null;
+  }
+
+
+
+  public checkMe() {
+    console.log("checkMe called");
+    if (window.getSelection) {
+        var sel  = window.getSelection()
+        console.log("Selected in window " + sel);
+    } 
+    if (document.getSelection) {
+      var sel  = window.getSelection()
+      console.log("Selected in document " + sel);
+    } 
+
+    
+  }
 }
